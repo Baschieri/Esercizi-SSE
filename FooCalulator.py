07fbc869d1298b34e1b08ef80216c7253fe3636e
Baschieri <baschierigabriele.lu@gmail.com>
2021-09-30 18:04:29 +0200

    Adding files

---
+++ FooCalulator.py
@@ -0,0 +1,13 @@
+import esercizio1 as c
+
+class FooCalculator:
+
+    #costruttore
+    def __init__(self):
+        pass
+        
+    def sum(self,m,n):
+        return c.sum(m,n)
+
+    def divide(seld,m,n):
+        return c.divide(m,n)
---
+++ main.py
@@ -0,0 +1,10 @@
+
+from FooCalulator import FooCalculator
+from esercizio1 import *
+
+
+obj=FooCalculator();
+print(obj.sum(1,2))
+print(obj.divide(4,2))
+
+
